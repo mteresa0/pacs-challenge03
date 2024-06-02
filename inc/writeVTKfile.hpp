e53fd0e09124a12ef5a7c62ac4c15ec9eac3ffc5
Teresa Cazzola <mteresacazzola@gmail.com>
2024-06-03 01:31:01 +0200

    update writeVTK - rectangular grids

--- inc/writeVTKfile.hpp
+++ inc/writeVTKfile.hpp
@@ -1,11 +1,12 @@
-#ifndef WRITE_VTK_HPP
-#define WRITE_VTK_HPP
+#ifndef WRITE_VTK_2D_HPP
+#define WRITE_VTK_2D_HPP
 
 #include <iostream>
 #include <fstream>
 #include "domain.hpp"
 
-void write_VTK(const unsigned int & N, const double & h, const std::vector<double> & data, const std::string & filename, const std::string & title = "data")
+void write_VTK_2D_domain(const unsigned int & Nx, const unsigned int Ny, const double & hx, const double & hy,
+               const std::vector<double> & data, const std::string & filename, const std::string & title = "data")
 {
     std::ofstream vtk_file(filename);
 
@@ -17,9 +18,10 @@ void write_VTK(const unsigned int & N, const double & h, const std::vector<doubl
     vtk_file << title <<"\nASCII\n";
 
     vtk_file << "DATASET STRUCTURED_POINTS\n";
-    vtk_file << "DIMENSIONS " << N << " " << N << " 1\n";
+    vtk_file << "DIMENSIONS " << Nx << " " << Ny << " 1\n";
     vtk_file << "ORIGIN 0.0 0.0 0.0\n";
-    vtk_file << "SPACING " << h << " " << h << " 1\n";
+    vtk_file << "SPACING " << hx << " " << hy << " 1\n";
+    if (data.size()!=Nx*Ny) std::cerr << "data size do not match dimensions of the grid";
     vtk_file << "POINT_DATA " << data.size() <<"\n";
     vtk_file << "SCALARS scalars double\n";
     vtk_file << "LOOKUP_TABLE default\n";
@@ -32,8 +34,16 @@ void write_VTK(const unsigned int & N, const double & h, const std::vector<doubl
     return;
 };
 
-void write_VTK(const laplacian_solver::Domain & domain ,const std::vector<double> & data, const std::string & filename, const std::string & title = "data"){
-    write_VTK(domain.N, domain.h, data, filename, title);
+void write_VTK_2D_domain(const unsigned int & N, const double & h, const std::vector<double> & data, const std::string & filename, const std::string & title = "data")
+{
+    write_VTK_2D_domain(N,N,h,h,data, filename, title);
+    return;
+}
+
+void write_VTK_2D_domain(const laplacian_solver::Domain & domain ,const std::vector<double> & data, const std::string & filename, const std::string & title = "data")
+{
+    write_VTK_2D_domain(domain.N, domain.h, data, filename, title);
+    return;
 };
 
-#endif // WRITE_VTK_HPP
+#endif // WRITE_VTK_2D_HPP
